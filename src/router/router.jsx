ff506a98e11fc840df3b5ae9e172623ff3e39eb9
sakibMERN <sakib.sr57@gmail.com>
2024-12-11 17:52:30 +0600

    project initial setup done

---
+++ src/router/router.jsx
@@ -0,0 +1,10 @@
+const { createBrowserRouter } = require("react-router-dom");
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello World!</div>
+    }
+]);
+
+export default router;
